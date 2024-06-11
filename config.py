c69aad0716bb24949d8506f0180500fbd4d0d9ce
Leo Carrico <lcarrico827@gmail.com>
2024-06-10 20:33:18 -0500

    oops removed config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-ALPACA_API_KEY = 'PK5BGQXJ5KWLLMTQ7WHK'
-ALPACA_API_SECRET = 'YwAXsYT3fdwD7YvG2lxQfDQEfhF30NQfXoGYzEC4'
-
-# PostgreSQL credentials
-DB_HOST = '192.168.0.169'
-DB_NAME = 'finance_db'
-DB_USER = 'lcarr'
-DB_PASS = 'TeddyBear22!'
